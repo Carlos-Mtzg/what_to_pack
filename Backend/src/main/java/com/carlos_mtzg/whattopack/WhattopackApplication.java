524e100edd2187d52664a262613669874cb1c751
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-05-28 19:54:06 -0600

    feat(backend): the backend project was created and initialized

---
+++ Backend/src/main/java/com/carlos_mtzg/whattopack/WhattopackApplication.java
@@ -0,0 +1,13 @@
+package com.carlos_mtzg.whattopack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattopackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhattopackApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/carlos_mtzg/whattopack/WhattopackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos_mtzg.whattopack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhattopackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
